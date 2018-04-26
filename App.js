42fdec6c517fe15c493785d3c2aa0b65a1bebe64
damian.kisielewski@gmail.com <Damian@MacBook-Pro-Damian.local>
2018-04-26 23:47:00 +0100

    Unnecessary files deleted

--- App.js
+++ App.js
@@ -5,9 +5,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>ODPALIŁEM </Text>
+        <Text>REACT NATIVE</Text>
       </View>
     );
   }
@@ -16,7 +15,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
