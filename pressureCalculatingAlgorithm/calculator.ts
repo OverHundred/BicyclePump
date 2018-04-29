1e9989d037b2a8f8ad9879e60508561cd6db38de
Dawid Kisielewski <dawidpol1@gmail.com>
2018-04-29 14:25:29 +0800

    Creating fully functional preassure calculator

--- pressureCalculatingAlgorithm/calculator.ts
+++ pressureCalculatingAlgorithm/calculator.ts
@@ -1,3 +1,39 @@
-export const calculate = () => {
-    return 1 + 1
+import { cloneDeep } from 'lodash'
+import { IPreassureCalculatorResult, IPressureCalculatorData } from './interfaces'
+import { validateInput } from './validateInput';
+
+export const calculatePreassure = (userData: IPressureCalculatorData): IPreassureCalculatorResult => {
+    validateInput(userData)
+    
+    let bikeWeight = userData.bikeWeight ? userData.bikeWeight : 10
+    let totalWeight = userData.riderWeight + bikeWeight
+
+    let a = 0.00384416 * Math.pow(userData.tireWidth, 2) - 0.3314 * userData.tireWidth + 8.04906//(57 / 9) - userData.tireWidth * (5 / 30)
+
+    //Weight distribiution is 40% front and 60% back
+    let frontWeight = 40 / 100 * totalWeight
+    let backWeight = 60 / 100 * totalWeight
+
+    let calculatedValues = {
+        front: a * frontWeight - 10,
+        back: a * backWeight - 10
+    }
+
+    return putPreassureBetweenMinAndMax(calculatedValues, userData)
 }
+
+export const putPreassureBetweenMinAndMax = (calculatedValues: IPreassureCalculatorResult, userData: IPressureCalculatorData) => {
+    let newCalculatedValues = cloneDeep(calculatedValues)
+
+    if (newCalculatedValues.front > userData.maxPreassure)
+        newCalculatedValues.front = userData.maxPreassure
+    else if (newCalculatedValues.front < userData.minPreassure)
+        newCalculatedValues.front = userData.minPreassure
+
+    if (newCalculatedValues.back > userData.maxPreassure)
+        newCalculatedValues.back = userData.maxPreassure
+    else if (newCalculatedValues.back < userData.minPreassure)
+        newCalculatedValues.back = userData.minPreassure
+
+    return newCalculatedValues
+} 
--- tests/pressureCalculatingAlgorithm/calculator.test.ts
+++ tests/pressureCalculatingAlgorithm/calculator.test.ts
@@ -1,8 +1,63 @@
-import { calculate } from '../../pressureCalculatingAlgorithm/calculator'
+import { calculatePreassure } from '../../pressureCalculatingAlgorithm/calculator'
+import { IPressureCalculatorData } from '../../pressureCalculatingAlgorithm/interfaces'
 
+describe('Preassure calculator', () => {
+    it('should not exceed maximum preassure', () => {
+        let userData: IPressureCalculatorData = {
+            tireWidth: 53.3,
+            riderWeight: 9999,
+            minPreassure: 40,
+            maxPreassure: 65
+        }
+    
+        let result = calculatePreassure(userData)
+        
+        let eps = 0.000000001
 
-it('should calculate', () => {
-    let result = calculate()
+        expect(result.back + eps >= 65 && result.back - eps <= 65).toEqual(true)
+        expect(result.front + eps >= 65 && result.front - eps <= 65).toEqual(true)
+    });
+    it('should not be smaller than minimum preassure', () => {
+        let userData: IPressureCalculatorData = {
+            tireWidth: 53.3,
+            riderWeight: 1,
+            minPreassure: 40,
+            maxPreassure: 65
+        }
+    
+        let result = calculatePreassure(userData)
+        
+        let eps = 0.000000001
 
-    expect(result).toEqual(2)
-});
+        expect(result.back + eps >= 40 && result.back - eps <= 40).toEqual(true)
+        expect(result.front + eps >= 40 && result.front - eps <= 40).toEqual(true)
+    });
+
+    it('should not exceed beetwen max and min', () => {
+        let userData: IPressureCalculatorData = {
+            tireWidth: 53.3,
+            riderWeight: 70,
+            minPreassure: 40,
+            maxPreassure: 65
+        }
+    
+        let result = calculatePreassure(userData)
+        
+        expect(result.back <= 65 && result.back >= 40).toEqual(true)
+        expect(result.front <= 65 && result.front >= 40).toEqual(true)
+    });
+
+    it('should not exceed beetwen max and min', () => {
+        let userData: IPressureCalculatorData = {
+            tireWidth: 53.3,
+            riderWeight: 70,
+            minPreassure: 40,
+            maxPreassure: 65
+        }
+    
+        let result = calculatePreassure(userData)
+        
+        expect(result.back <= 65 && result.back >= 40).toEqual(true)
+        expect(result.front <= 65 && result.front >= 40).toEqual(true)
+    });
+})
