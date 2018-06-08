4c8602edf45f52773c6319e8fcb627e2b3aa2d3e
damian.kisielewski@gmail.com <Damian@MacBook-Pro-Damian.local>
2018-06-08 22:11:03 +0100

    Ui changes

--- components/slider/bpSlider.tsx
+++ components/slider/bpSlider.tsx
@@ -2,49 +2,72 @@
 import React from 'react';
 import { StyleSheet, Text, View, Slider } from 'react-native';
 
-export class BPSlider extends React.Component {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            value: props.value,
-        }
+const BPSlider = props => {
+  const handleValueChanged = value => {
+    if ( props.onValueChange ) {
+      props.onValueChange(value)
     }
-    
-    handleValueChanged = (newValue) => {
-        this.setState({ value: newValue })
-
-        if (this.props.onValueChange)
-            this.props.onValueChange(newValue)
-
-    }
-
-    render(): any {
-        return (
-            <View style={styles.container}>
-                <Text >{this.state.value}</Text>
-                <View style={styles.sliderContainer}>
-                    <Slider
-                        maximumValue={100}
-                        minimumValue={0}
-                        step={1}
-                        value={this.state.value}
-                        onValueChange={ this.handleValueChanged }
-                    />
-                </View>
-            </View>
-        );
-    }
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.labelContainer} >
+        <Text style={styles.label}>{ props.title }</Text>
+        <View style={styles.valueContainer}> 
+          <Text style={styles.value}>{props.value}</Text>
+          <Text style={styles.units}> {props.subtitle}</Text>
+        </View>
+      </View>
+        <Slider
+          style={styles.slider}
+          maximumValue={props.max}
+          minimumValue={props.min}
+          step={1}
+          value={props.initValue}
+          onValueChange={ handleValueChanged }
+        />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '100%'
-      
+      width: '100%',
+      height: '20%',
+      padding: 0,
     },
-    sliderContainer: {
-      width: '100%'
+    slider: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 0,
+      margin: 0,
+      height: 30,
+    },
+    labelContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingRight: 20,
+      paddingLeft: 20,
+    },
+    label: {
+      color: "white",
+      fontSize: 24,
+    },
+    units : {
+      color: "#434343",
+      alignSelf: 'center',
+      fontSize: 16,
+      paddingLeft: 5,
+    },
+    valueContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    value: {
+      fontSize: 24,
+      color: "white"
     }
   });
+
+export default BPSlider;
